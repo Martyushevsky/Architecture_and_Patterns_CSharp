12ee1a367ff56e957591df12c7f391c71ea82945
Unknown <martyushevsky@gmail.com>
2018-04-14 23:04:40 +0300

    Lesson4

---
+++ Lesson4_Task1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace Lesson4_Task1
+{
+    public class Singleton
+    {
+        public int threadTest = 0;
+
+        private static Singleton _instance;
+        private static Object _locker = new Object();
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_locker)
+                {
+                    if (_instance == null)
+                        _instance = new Singleton();
+                }
+                //_instance = new Singleton();
+            }
+            return _instance;
+        }
+
+        public void Test()
+        {
+            threadTest++;
+        }
+
+        private Singleton() { }
+    }
+
+    class Program
+    {
+        static void NewThread()
+        {
+            Singleton singleton = Singleton.GetInstance();
+            singleton.Test();
+            Console.WriteLine("{0}", singleton.threadTest);
+        }
+
+        static void Main(string[] args)
+        {
+            var threads = new Thread[10];
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(NewThread);
+                threads[i].Start();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson4_Task2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace Lesson4_Task2
+{
+    public class Singleton
+    {
+        public int threadTest = 0;
+
+        private static Lazy<Singleton> lazy = new Lazy<Singleton>(() => new Singleton());
+
+        public static Singleton GetInstance()
+        {
+            return lazy.Value;
+        }
+
+        public void Test()
+        {
+            threadTest++;
+        }
+
+        private Singleton() { }
+    }
+
+    class Program
+    {
+        static void NewThread()
+        {
+            Singleton singleton = Singleton.GetInstance();
+            singleton.Test();
+            Console.WriteLine("{0}", singleton.threadTest);
+        }
+
+        static void Main(string[] args)
+        {
+            var threads = new Thread[10];
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(NewThread);
+                threads[i].Start();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

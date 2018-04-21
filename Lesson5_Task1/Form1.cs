ce42850628182a3dad4f59a871d3740bad362a1a
Unknown <martyushevsky@gmail.com>
2018-04-22 01:52:06 +0300

    Lesson5

---
+++ Lesson5_Task1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lesson5_Task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Random r = new Random();
+
+            for (int i = 0; i < 50_000; i++)
+            {
+                DrawShape ellipse = ShapeFactory.GetShape("Ellipse");
+                ellipse.Draw(new Point(r.Next(Width / 2), r.Next(Height / 2)), new Point(r.Next(Width / 2), r.Next(Height / 2)));
+            }
+
+            //for (int i = 0; i < 50_000; i++)
+            //{
+            //    Pen pen = Pens.Black;
+            //    Graphics g = CreateGraphics();
+            //    g.DrawEllipse(pen, r.Next(Width / 2), r.Next(Height / 2), r.Next(Width / 2), r.Next(Height / 2));
+            //}
+        }
+    }
+
+    public abstract class DrawShape
+    {
+        public Pen pen = Pens.Black;
+        public Graphics graphics = Form.ActiveForm.CreateGraphics();
+
+        public abstract void Draw(Point originPoint, Point dimensions);
+    }
+
+    class DrawEllipse : DrawShape
+    {
+        public override void Draw(Point originPoint, Point dimensions)
+        {
+            graphics.DrawEllipse(pen, originPoint.X, originPoint.Y, dimensions.X, dimensions.Y);
+        }
+    }
+
+    class DrawRectangle : DrawShape
+    {
+        public override void Draw(Point originPoint, Point dimensions)
+        {
+            graphics.DrawRectangle(pen, originPoint.X, originPoint.Y, dimensions.X, dimensions.Y);
+        }
+    }
+
+    public class ShapeFactory
+    {
+        static Dictionary<string, DrawShape> shapes = new Dictionary<string, DrawShape>();
+
+        public static DrawShape GetShape(string shapeType)
+        {
+            switch (shapeType)
+            {
+                case "Ellipse":
+                    if (!shapes.ContainsKey("Ellipse"))
+                        shapes["Ellipse"] = new DrawEllipse();
+                    return shapes["Ellipse"];
+                case "Rectangle":
+                    if (!shapes.ContainsKey("Rectangle"))
+                        shapes["Rectangle"] = new DrawRectangle();
+                    return shapes["Rectangle"];
+                default:
+                    break;
+            }
+            return null;
+        }
+    }
+}
---
+++ Lesson5_Task2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Lesson5_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMath operation = new MathProxy();
+
+            operation.Add(1024, 128);
+            operation.Subtract(512, 64);
+            operation.Multiply(32, 8);
+            operation.Divide(4096, 16);
+
+            Console.ReadLine();
+        }
+    }
+
+    public interface IMath
+    {
+        int Add(int a, int b);
+        int Subtract(int a, int b);
+        int Multiply(int a, int b);
+        int Divide(int a, int b);
+    }
+
+    class Math : IMath
+    {
+        public int Add(int a, int b) { return a + b; }
+        public int Subtract(int a, int b) { return a - b; }
+        public int Multiply(int a, int b) { return a * b; }
+        public int Divide(int a, int b) { return a / b; }
+    }
+
+    class MathProxy : IMath
+    {
+        Math math;
+
+        public int Add(int a, int b)
+        {
+            if (math == null)
+                math = new Math();
+
+            int c = math.Add(a, b);
+            Console.WriteLine("{0} + {1} = {2}", a, b, c);
+            return c;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            if (math == null)
+                math = new Math();
+
+            int c = math.Subtract(a, b);
+            Console.WriteLine("{0} - {1} = {2}", a, b, c);
+            return c;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            if (math == null)
+                math = new Math();
+
+            int c = math.Multiply(a, b);
+            Console.WriteLine("{0} * {1} = {2}", a, b, c);
+            return c;
+        }
+
+        public int Divide(int a, int b)
+        {
+            if (math == null)
+                math = new Math();
+
+            int c = math.Divide(a, b);
+            Console.WriteLine("{0} / {1} = {2}", a, b, c);
+            return c;
+        }
+    }
+}
